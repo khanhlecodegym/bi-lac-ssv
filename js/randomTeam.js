afd576e77284eb58284732e5397e46673308a70e
HoangPT <hoangpt@saisystem.vn>
2021-07-21 13:31:20 +0700

    AI

--- js/randomTeam.js
+++ js/randomTeam.js
@@ -7,15 +7,18 @@ function randomTeam(arrayNames, i)
     var element2 = document.getElementById("btnClick");
     element2.classList.add("btnDisplay");
 
+    var img_hacker = document.getElementById("bodyID");
+    img_hacker.classList.remove("backGround");
+
     var element = document.getElementById("myDIV");
     element.classList.add("loader");
     setTimeout(() => {
         let random = Math.floor(Math.random() * arrayNames.length);
         if(i%2 ==0){
             $('#dataAppen1').append(`
-                <tr>
+                <tr class="imgTeam" >
                     <td class="team-name">
-                    <img src="img/number/number${i}.png" alt="Đội cá mập" />
+                    <img src="img/number/number${i}.png" style="margin-left:20px"/>
                     <span>${arrayNames[random]}</span>
                     </td>
                 </tr>
@@ -23,9 +26,9 @@ function randomTeam(arrayNames, i)
             `);
         }else{
             $('#dataAppen2').append(`
-                <tr>
+                <tr class="imgTeam" >
                     <td class="team-name">
-                    <img src="img/number/number${i}.png" alt="Đội cá mập" />
+                    <img src="img/number/number${i}.png" alt="Đội cá mập" style="margin-left:20px"/>
                     <span>${arrayNames[random]}</span>
                     </td>
                 </tr>
@@ -37,12 +40,13 @@ function randomTeam(arrayNames, i)
         arrayNames = arrayNames.filter(item => item !== arrayNames[random]);
 
         if(arrayNames.length == 0){
+            img_hacker.classList.add("backGround");
             element.classList.remove("loader");
             element.innerHTML = 'PHẦN MỀM SỬ DỤNG TRÍ TUỆ NHÂN TẠO 2.0 - KHÔNG TIẾP NHẬN Ý KIẾN';
             element.classList.add("myload");
             return false;
         }
         return randomTeam(arrayNames,i);
-    }, 3500);
+    }, 2000);
 
 }
